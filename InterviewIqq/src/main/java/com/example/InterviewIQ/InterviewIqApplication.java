0a03f6a33fff3245e02e3daa91e477fe90348e9b
vidishaa09 <vidishaarora14@gmail.com>
2026-07-06 22:21:17 +0530

    Initial Spring Boot project setup

---
+++ InterviewIqq/src/main/java/com/example/InterviewIQ/InterviewIqApplication.java
@@ -0,0 +1,13 @@
+package com.example.InterviewIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewIqApplication.class, args);
+	}
+
+}
---
+++ InterviewIqq/src/test/java/com/example/InterviewIQ/InterviewIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InterviewIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
