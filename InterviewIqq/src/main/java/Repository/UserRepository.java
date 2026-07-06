16c30194a5a4ff3576da1e47604c124bc4160805
vidishaa09 <vidishaarora14@gmail.com>
2026-07-07 00:07:49 +0530

    Create Users Repository

---
+++ InterviewIqq/src/main/java/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package Repository;
+
+import Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
