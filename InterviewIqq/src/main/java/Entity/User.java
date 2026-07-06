7e89f43c4014227012afe3d94535ac2aa3c1af20
vidishaa09 <vidishaarora14@gmail.com>
2026-07-07 00:07:20 +0530

    Add Users Entity

---
+++ InterviewIqq/src/main/java/Entity/User.java
@@ -0,0 +1,43 @@
+package Entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="full_name" , nullable=false)
+    private String fullName;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at" , nullable = false)
+    private LocalDateTime updatedAt;
+
+
+
+}
