ad792d0ba22df88e44a159a951a1c1cfe0250f01
vidishaa09 <vidishaarora14@gmail.com>
2026-07-07 00:06:07 +0530

    Initial Spring Boot project setup

--- InterviewIqq/src/main/java/com/example/InterviewIQ/InterviewIqApplication.java
+++ InterviewIqq/src/main/java/com/example/InterviewIQ/InterviewIqApplication.java
@@ -3,10 +3,14 @@ package com.example.InterviewIQ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InterviewIqApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(InterviewIqApplication.class, args);
 	}
 
